dc34b279227c0be3312929cc44f0d486b38231c1
A. Bram Neijt <bneijt@gmail.com>
2010-08-01 15:43:01 +0200

    First working code

--- src/main/scala/net/logfish/animated-background-builder/Application.scala
+++ src/main/scala/net/logfish/animated-background-builder/Application.scala
@@ -18,7 +18,7 @@ object Application {
         val shiftedImages = images.tail ++ List(images.head)
         val imageNodes = images map (fileName => <static><duration>{staticDuration}</duration><file>{fileName}</file></static> )
         val transitionNodes = (images zip shiftedImages) map ( i => <transition><duration>{transitionDuration}</duration><from>{i._1}</from><to>{i._2}</to></transition> )
-        val nodes = imageNodes zip (transitionNodes) flatten()
+        val nodes = List.flatten((imageNodes zip transitionNodes) map ( i => List(i._1, i._2)) toList)
         val background = <background>{ nodes }</background>
         println(background)
         System.exit(0)
